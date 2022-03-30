2ae065179a8dd4991ac7e8eccda308e323cdcd18
subhodeepmoitra <subhodeep2000@gmail.com>
2022-03-30 09:14:09 +0530

    fa fa icons bug fixed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,15 +8,13 @@
   <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
   <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
 
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+  <link rel="stylesheet" type="text/css" href="{{URL::asset('https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal')}}">
   <link rel="stylesheet" type="text/css" href="{{URL::asset('index/css/font-awesome.min.css')}}">
   <link rel="stylesheet" type="text/css" href="{{URL::asset('index/css/bootstrap.min.css')}}">
   <link rel="stylesheet" type="text/css" href="{{URL::asset('index/css/style.css')}}">
   <!-- =======================================================
     Theme Name: Medilab
-    Theme URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
-    Author: BootstrapMade.com
-    Author URL: https://bootstrapmade.com
+    Author: Rituparna Das
   ======================================================= -->
 </head>
 
